0ecc1b3820466c0df537d40427c37c6773561a67
Abhishek Adhikari <abhiayush23@gmail.com>
2023-05-30 19:18:10 +0530

    wireit setup

--- src/api/media.js
+++ src/api/media.js
@@ -1,5 +1,6 @@
 const mediaRouter = require("express").Router();
 
+// TODO: Add post route to setup db entry
 mediaRouter.post("/create", (req, res) => {});
 
 module.exports = mediaRouter;
--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const path = require("path"),
   mongoose = require("mongoose");
-global.appRoot = path.join(path.resolve(__dirname), "..");
 
-console.log(appRoot);
+// TODO: Task 1
+
+global.appRoot = path.join(path.resolve(__dirname), "..");
